172fac87dcdc9ecd77e9c77bcfb613876653ba3d
Igor <30476065+Igor-san@users.noreply.github.com>
2024-01-08 17:45:54 +0300

    11.31 Assisted Inject

--- app/src/main/java/com/sumin/weatherapp/presentation/details/DefaultDetailsComponent.kt
+++ app/src/main/java/com/sumin/weatherapp/presentation/details/DefaultDetailsComponent.kt
@@ -10,12 +10,17 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import com.arkivanov.mvikotlin.extensions.coroutines.labels
-class DefaultDetailsComponent @Inject constructor(
-    private val city: City,
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+
+class DefaultDetailsComponent @AssistedInject constructor(
     private val storeFactory: DetailsStoreFactory,
-    private val onBackClicked: () -> Unit,
-    componentContext: ComponentContext
+    @Assisted("city") private val city: City,
+    @Assisted("onBackClicked") private val onBackClicked: () -> Unit,
+    @Assisted("componentContext") componentContext: ComponentContext
 ) : DetailsComponent, ComponentContext by componentContext {
+
 
     private val store = instanceKeeper.getStore { storeFactory.create(city) }
     private val scope = componentScope()
@@ -41,5 +46,14 @@ class DefaultDetailsComponent @Inject constructor(
 
     override fun onClickChangeFavouriteStatus() {
         store.accept(DetailsStore.Intent.ClickChangeFavouriteStatus)
+    }
+    @AssistedFactory
+    interface Factory {
+
+        fun create(
+            @Assisted("city") city: City,
+            @Assisted("onBackClicked") onBackClicked: () -> Unit,
+            @Assisted("componentContext") componentContext: ComponentContext
+        ): DefaultDetailsComponent
     }
 }
